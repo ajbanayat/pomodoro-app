4382153579a3d726bb0351f37c644d10c58cf1e9
Allison <53547310+ajbanayat@users.noreply.github.com>
2025-07-30 18:12:31 -0500

    Removed updateSeconds from the startSession method

--- app.js
+++ app.js
@@ -7,38 +7,40 @@ const seconds = document.querySelector('.seconds');
 
 let intervalID;
 let isRunning = false;
+let currentTimeInSeconds;
 
 const startSession = () => {
-    let currentTimeInSeconds = (Number.parseInt(minutes.textContent) * 60) + Number.parseInt(seconds.textContent);
+    currentTimeInSeconds = (Number.parseInt(minutes.textContent) * 60) + Number.parseInt(seconds.textContent);
 
     if (!isRunning) {
         isRunning = true;
         startBtn.classList.add("hidden");
         pauseBtn.classList.remove("hidden");
 
-        const updateSeconds = () => {
-            const minuteDiv = document.querySelector(".minutes");
-            const secondDiv = document.querySelector(".seconds");
-
-            currentTimeInSeconds--;
-            let timeLeftInMinutes = Math.floor(currentTimeInSeconds/60);
-            let timeLeftInSeconds = currentTimeInSeconds % 60;
-
-            if (timeLeftInSeconds < 10) {
-                secondDiv.textContent = "0" + timeLeftInSeconds;
-            } else {
-                secondDiv.textContent = timeLeftInSeconds;
-            }
-            minuteDiv.textContent = timeLeftInMinutes;
-
-            if (timeLeftInMinutes === 0 && timeLeftInSeconds === 0) {
-                bells.play();
-                clearInterval(intervalID);
-            }
-        }
         intervalID = setInterval(updateSeconds, 1000);
     } else {
         alert("Session has already started.");
+    }
+}
+
+const updateSeconds = () => {
+    const minuteDiv = document.querySelector(".minutes");
+    const secondDiv = document.querySelector(".seconds");
+
+    currentTimeInSeconds--;
+    let timeLeftInMinutes = Math.floor(currentTimeInSeconds/60);
+    let timeLeftInSeconds = currentTimeInSeconds % 60;
+
+    if (timeLeftInSeconds < 10) {
+        secondDiv.textContent = "0" + timeLeftInSeconds;
+    } else {
+        secondDiv.textContent = timeLeftInSeconds;
+    }
+    minuteDiv.textContent = timeLeftInMinutes;
+
+    if (timeLeftInMinutes === 0 && timeLeftInSeconds === 0) {
+        bells.play();
+        clearInterval(intervalID);
     }
 }
 
